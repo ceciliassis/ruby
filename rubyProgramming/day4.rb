5036e6551d23582aebea9ae0b03b4a5954d8eeab
ceciliassis <cy_assis@hotmail.com>
2016-11-24 01:09:29 -0200

    array and string

--- rubyProgramming/day4.rb
+++ rubyProgramming/day4.rb
@@ -1,4 +1,4 @@
-# Basic formatting
+# Basic formatting adn string
 
 my_name = 'ceciliassis'
 
@@ -10,4 +10,19 @@ puts my_name.length # quantidade de letras na string
 
 star_wars_seven = 'Jar Jar Binks Han Solo Luke lens flare'
 puts star_wars_seven[14..26] # vetor normal, comecando de zero
-puts star_wars_seven[15] #somente uma posicao no vetor
+puts star_wars_seven[15] # somente uma posicao no vetor
+
+puts star_wars_seven['Jar']
+puts star_wars_seven * 2
+puts star_wars_seven['Lea'] # blank
+
+################################ let's play
+stirg = 'Oi' 'como' 'vai' # implicit string concatention
+puts stirg
+
+stirg = 'Oi' << 'como' << 'vai' # implicit string concatention
+puts stirg
+
+stirg = 'oi'.concat('como').concat('vai')
+puts stirg
+puts stirg.index('oi')
---
+++ rubyProgramming/day5and6.rb
@@ -0,0 +1,42 @@
+var = 'Mary Kate Olson Lindsay Lohan Charlie Sheen'
+puts var
+puts var.split
+
+date = '11/11/2016'
+date2 = date.split '/' # an array
+puts date2[0]
+puts date[0..5] # inclui o 5
+puts date[0...5] # nao inclui o ultimo char sengundo o indice
+
+array = %w(brady partridge bundy) # better for strings
+# array = ['brady', 'partridge', 'bundy'] #know
+
+puts array.size # same as
+puts array.length
+
+puts array[0]
+puts array[-3] # so bundy -1  ... brady -3
+
+array = ['brady', 6, false, true, 'cordova', 6 - 1]
+
+puts array[-1]
+puts array[3].class # saida TrueClass
+puts array[2].class # saida FalseClass
+
+array = %w(brady 6 true cordova 6 - 1) # the will all be strings
+# here, each element turns into a string
+puts array[-1].class
+puts array[4] + array[-2] + array[array.size - 1] # saida 6-1
+puts 'The result is not ' + array[1]
+
+puts "conct with #{array[0]}" # make sure you use "" (double qoutes)
+
+########################## lets play
+
+array += ['name', 6] # concat array
+
+puts array
+puts array[0.9] # pega a parte inteira par indice
+puts array[0, 3] # 0 -> 2
+puts array[1..-5] # array[1] -> array[-5]
+puts array[-6..-3] # anda para 'frente'
