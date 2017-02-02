4134b346022457543941aea3e75975b2bc24ba8a
ceciliassis <cy_assis@hotmail.com>
2017-02-01 23:38:26 -0200

    ternary op

---
+++ rubyProgramming/day25.rb
@@ -0,0 +1,20 @@
+# one lines / operador teraário
+
+if 4 < 12
+  puts 'that is correct'
+end
+
+# if modifier
+puts 'that is correct' if 40 < 12
+
+# 50 is not less than 20
+puts 'i like cheese!' unless 50 < 20
+
+# operador ternario
+if 40 < 12
+  puts 'yes, it is'
+else
+  puts 'no it isnt'
+end
+
+puts 40 < 200 ? 'yes it is' : 'no it is not'
