847b8271fd5a7611113cb1d4d0d2f01130ee0e14
Cecília Assis <cy_assis@hotmail.com>
2019-10-22 10:31:50 -0300

    Update git-alias.sh

--- RandomCodes/git-alias.sh
+++ RandomCodes/git-alias.sh
@@ -3,15 +3,26 @@
 #git config --global alias.br branch
 #git config --global alias.cm commit
 
-alias gco="git checkout"
 alias glg="git log"
+alias gcm="git commit"
 alias gst="git status"
+alias gco="git checkout"
 alias gps="git push"
 alias gmg="git merge"
-alias gcm="git commit"
 alias gad="git add"
 alias gbr="git branch"
- 
+alias gpl="git pull"
+
+_completion_loader git log
+_completion_loader git commit
+_completion_loader git status
+_completion_loader git checkout
+_completion_loader git push
+_completion_loader git merge
+_completion_loader git add
+_completion_loader git branch
+_completion_loader git pull
+
 complete -F _git_log glg
 complete -F _git_commit gcm
 complete -F _git_status gst
@@ -20,3 +31,4 @@ complete -F _git_push gps
 complete -F _git_merge gmg
 complete -F _git_add gad
 complete -F _git_branch gbr
+complete -F _git_pull gpl
